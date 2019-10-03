fe968c2e50561657830eca0006af1011400dbda7
sdgandhi <sdgandhi@users.noreply.github.com>
2019-10-03 12:59:51 -0700

    Add enable/disable to popup

--- webext/src/popup/popup.js
+++ webext/src/popup/popup.js
@@ -1,3 +1,45 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './popup.css'
+
+class Popup extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      enabled: false
+    }
+
+    chrome.storage.sync.get('enabled', (data) => {
+      this.setState({
+        enabled: data.enabled
+      })
+    })
+  }
+
+  render () {
+    return (
+      <React.Fragment>
+        <h1>80</h1>
+        <p>This webpage is trustworthy because...</p>
+        <input type="checkbox" checked={ this.state.enabled } onChange={(target) => {
+          chrome.storage.sync.set({ enabled: !this.state.enabled }, () => {
+            this.setState((prevState) => ({
+              enabled: !prevState.enabled
+            }), () => {
+              chrome.tabs.reload()
+            })
+          })
+        }} />
+        <label>Enabled</label>
+        <script src="popup.js"></script>
+      </React.Fragment>
+    )
+  }
+}
+
+ReactDOM.render(<Popup />, document.getElementById('popup'))
+
+/*
 const changeColor = document.getElementById('changeColor')
 
 chrome.storage.sync.get('color', function (data) {
@@ -14,3 +56,4 @@ changeColor.onclick = function (element) {
     )
   })
 }
+*/
