321ef9c6dad542d86fdb76e5b64587ef0caab9c6
sdgandhi <sdgandhi@users.noreply.github.com>
2019-10-04 10:13:30 -0700

    Two text elements so we can fade in one after another

--- webext/src/content/Bar.js
+++ webext/src/content/Bar.js
@@ -8,33 +8,37 @@ const textColor = 'lightseagreen'
 const textPadding = 3
 const fontSize = 12
 
+const animationDelay = 2000
+
 // Create styled components out here so we don't recreate on each render
 const Bar = styled.div`
-      pointer-events: none;
-      width: 100%;
-      height: ${barSize}px;
-      z-index: 99999;
-      background-color: ${barColor};
-      
-      :hover {
-        /* add hover styles */
-      }
-    `
+  pointer-events: none;
+  width: 100%;
+  height: ${barSize}px;
+  z-index: 99999;
+  background-color: ${barColor};
+  
+  :hover {
+    /* add hover styles */
+  }
+`
 
 const Text = posed.div({
-  hidden: { opacity: 0, y: 5, transition: { duration: 700 } },
-  visible: { opacity: 1, y: 0, transition: { duration: 700 } }
+  hidden: { opacity: 0, y: 5, transition: { duration: 500 } },
+  visible: { opacity: 1, y: 0, transition: { duration: 500 } }
 })
 
 const BarText = styled(Text)`
-      pointer-events: none;
-      padding: ${textPadding}px;
-      font-family: monospace;
-      font-size: ${fontSize}px;
-      line-height: ${fontSize}px;
-      color: ${textColor};
-      text-align: center;
-    `
+  position: fixed;
+  width: 100%;
+  pointer-events: none;
+  padding: ${textPadding}px;
+  font-family: monospace;
+  font-size: ${fontSize}px;
+  line-height: ${fontSize}px;
+  color: ${textColor};
+  text-align: center;
+`
 
 export default class BarComponent extends React.Component {
   constructor () {
@@ -51,12 +55,12 @@ export default class BarComponent extends React.Component {
       this.setState((prevState) => {
         let newIndex = prevState.textIndex
         if (!prevState.textVisible) {
-          newIndex = (prevState.textIndex + 1) % prevState.textLines.length
+          newIndex = (prevState.textIndex + 2) % prevState.textLines.length
         }
 
         return { textIndex: newIndex, textVisible: !prevState.textVisible }
       })
-    }, 1000)
+    }, animationDelay)
 
     this.setState({
       textTimer: timer
@@ -75,6 +79,9 @@ export default class BarComponent extends React.Component {
       <Bar>
         <BarText pose={ textVisible ? 'visible' : 'hidden'}>
           {textLines[textIndex]}
+        </BarText>
+        <BarText pose={textVisible ? 'hidden' : 'visible'}>
+          {textLines[(textIndex + 1) % textLines.length]}
         </BarText>
       </Bar>
     )
