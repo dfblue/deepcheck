63b834c2a951682a274cc53d20d6fc695dcb3d18
sdgandhi <sdgandhi@users.noreply.github.com>
2019-10-21 14:39:28 -0700

    Publish during deploy also

--- deploy.js
+++ deploy.js
@@ -7,25 +7,39 @@ const chromeWebstore = require('chrome-webstore-upload')
 
 require('dotenv').config()
 
-const {
-  client_id,
-  client_secret,
-  refresh_token,
-  item_id
-} = process.env
+const distributionGroup = process.argv[2]
+if (distributionGroup) {
+  const argErrorMessage = '1st argument should specify distribution group "default" or "trustedTesters"'
+  assert(distributionGroup.match(/(default|trustedTesters)/g), argErrorMessage)
+}
 
-assert(process.argv.length === 3, 'Second argument should be the path of the zip file')
-const zipPath = process.argv[2]
-const zipFile = fs.createReadStream(zipPath)
+const main = async () => {
+  const {
+    client_id,
+    client_secret,
+    refresh_token,
+    item_id
+  } = process.env
 
-const webstoreClient = chromeWebstore({
-  extensionId: item_id,
-  clientId: client_id,
-  clientSecret: client_secret,
-  refreshToken: refresh_token
-})
+  assert(process.argv.length === 3, 'Second argument should be the path of the zip file')
+  const zipPath = process.argv[2]
+  const zipFile = fs.createReadStream(zipPath)
 
-webstoreClient.uploadExisting(zipFile).then((res) => {
+  const webstoreClient = chromeWebstore({
+    extensionId: item_id,
+    clientId: client_id,
+    clientSecret: client_secret,
+    refreshToken: refresh_token
+  })
+
+  const upload = await webstoreClient.uploadExisting(zipFile)
   // https://developer.chrome.com/webstore/webstore_api/items#resource
-  console.log(`\n\nUpload state: ${res.uploadState}\nUpload errors: ${(res.itemError || []).map(e => JSON.stringify(e)).join('\n\t')}`)
-})
+  console.log(`\n\nUpload state: ${upload.uploadState}\nUpload errors: ${(upload.itemError || []).map(e => JSON.stringify(e)).join('\n\t')}`)
+  console.log('\n\nPublishing new version...')
+  // https://developer.chrome.com/webstore/webstore_api/items/publish
+  const publish = await webstoreClient.publish(distributionGroup)
+  console.log(`\n\nUpload status: ${publish.status.join('\t')}`)
+  console.log(`\n\nUpload details: ${publish.statusDetail.join('\t')}`)
+}
+
+main().then()
