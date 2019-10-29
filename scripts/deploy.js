47664532454fbbab844a778f6c00fe746845fc1b
sdgandhi <sdgandhi@users.noreply.github.com>
2019-10-29 15:54:33 -0700

    Add commit message notes to version record

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,9 +3,12 @@
 /* eslint-disable camelcase */
 const assert = require('assert')
 const fs = require('fs')
+const path = require('path')
 const chromeWebstore = require('chrome-webstore-upload')
 const manifest = require('../src/manifest.json')
 const coda = require('./coda')
+const gitWebext = require('simple-git/promise')(path.join(__dirname, '..'))
+const gitServer = require('simple-git/promise')(path.join(__dirname, '../../deepcheck-server'))
 
 require('dotenv').config()
 
@@ -38,6 +41,17 @@ const main = async () => {
   }
 
   console.log('\n\nRecording version in Coda...')
+  // Get commit messages between this version and last
+  const gits = { extension: gitWebext, server: gitServer }
+  const notes = await Promise.all(Object.entries(gits).map(async ([name, git]) => {
+    const tags = (await git.tags()).all
+    const from = tags[tags.length - 2]
+    const to = tags[tags.length - 1]
+    const changes = (await git.log({ to, from })).all.slice(1)
+    const lines = [`**${name}**`]
+    lines.push(...changes.map(c => `* [${c.author_email}] ${c.message}`))
+    return lines
+  }))
   const versionsDoc = 'LXurD07hoa'
   const versionsTable = 'versions'
   const versionData = {
@@ -47,7 +61,8 @@ const main = async () => {
           { column: 'date', value: new Date() },
           { column: 'version', value: manifest.version },
           { column: 'status', value: upload.uploadState },
-          { column: 'store_link', value: `https://chrome.google.com/webstore/detail/deepcheck/${item_id}` }
+          { column: 'store_link', value: `https://chrome.google.com/webstore/detail/deepcheck/${item_id}` },
+          { column: 'notes', value: notes.join('\n') }
         ]
       }
     ]
